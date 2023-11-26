4450ce005e20e05b154899014612c36c329beac5
Deinigu <diegolopezreduello@gmail.com>
2023-11-26 19:48:08 +0100

    ✨Init inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './features/inicio/inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : '',
+        component : InicioComponent,
+        title : 'Inicio'
+    }
+];
