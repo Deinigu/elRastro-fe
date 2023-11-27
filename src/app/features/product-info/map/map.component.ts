d7923bb34a3b5d243ba2d15fc692eb04357a0a12
Ravenrex10 <lucas.colbert.eastgate@gmail.com>
2023-11-27 18:44:36 +0100

    Mapa funcionando

--- src/app/features/product-info/map/map.component.ts
+++ src/app/features/product-info/map/map.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import * as L from 'leaflet';
+
 @Component({
   selector: 'app-map',
   standalone: true,
@@ -9,5 +11,23 @@ import { CommonModule } from '@angular/common';
   styleUrl: './map.component.css'
 })
 export class MapComponent {
+  ngOnInit(): void {
+    const location = { latitude: 36.71781, longitude: -4.28771 };
+    this.initMap(location);
+  }
 
+  private initMap(location: { latitude: number; longitude: number }): void {
+    const map = L.map('map').setView([location.latitude, location.longitude], 30);
+  
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'  
+    }).addTo(map);
+  
+    L.marker([location.latitude, location.longitude])
+      .addTo(map)
+      .bindPopup('Specified Location')
+      .openPopup();
+  }
+  
 }
