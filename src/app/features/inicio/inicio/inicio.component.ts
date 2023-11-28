af0b109be3a48ef9b89efd5cdd5c8cac44b96176
Diego <diegolopezreduello@gmail.com>
2023-11-28 21:02:25 +0100

    ✨ Estructura terminada

--- src/app/features/inicio/inicio/inicio.component.ts
+++ src/app/features/inicio/inicio/inicio.component.ts
@@ -5,9 +5,26 @@ import { MaterialModule } from '../../../material.module';
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [CommonModule,MaterialModule],
+  imports: [CommonModule, MaterialModule],
   templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.css'
+  styleUrl: './inicio.component.css',
 })
 export class InicioComponent {
+  productos = [
+    {
+      Nombre: 'manolo',
+      descripcion: 'soy manolo',
+      fotoURL: 'https://i.imgur.com/PIOafIe.jpeg',
+    },
+    {
+      Nombre: 'paco',
+      descripcion: 'soy paco',
+      fotoURL: 'https://i.imgur.com/EnNBJtR.jpeg',
+    },
+    {
+      Nombre: 'sapo',
+      descripcion: 'soy sapo',
+      fotoURL: 'https://i.imgur.com/yz6eVNk.jpeg',
+    },
+  ];
 }
