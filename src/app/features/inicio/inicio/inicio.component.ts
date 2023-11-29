98c174ad329e598e4070942ece80312b2f857f2e
Diego <diegolopezreduello@gmail.com>
2023-11-29 20:18:13 +0100

    ✨ HTTP GET API PRODUCTOS

--- src/app/features/inicio/inicio/inicio.component.ts
+++ src/app/features/inicio/inicio/inicio.component.ts
@@ -2,21 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../../../material.module';
 import { ProductService } from '../../../services/product-service.service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, HttpClientModule],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css',
   providers: [ProductService]
 })
 export class InicioComponent implements OnInit{
 
+  productos : any[] = [];
+
   constructor(private http: HttpClient, private productService: ProductService){}
 
-  productos : any;
 
   ngOnInit(): void {
     this.productService.getAllProducts().subscribe(data => {
