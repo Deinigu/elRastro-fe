6cabe8384a2236ac0f01ba34d6baf1c6c71a01c5
Ravenrex10 <lucas.colbert.eastgate@gmail.com>
2023-12-06 18:42:41 +0100

    los mapas se ven bien

--- src/app/features/product-info/map/map.component.ts
+++ src/app/features/product-info/map/map.component.ts
@@ -2,31 +2,52 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import * as L from 'leaflet';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { UsuarioService } from '../../../services/usuario-service/usuario.service';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../../../services/product-service/product.service';
 
 @Component({
   selector: 'app-map',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './map.component.html',
-  styleUrl: './map.component.css'
+  styleUrl: './map.component.css',
+  providers: [UsuarioService, ProductService]
 })
 export class MapComponent {
+  idUsuario = "";
+  idProducto = "";
+  location = {latitude: 0, longitude: 0};
+
+  constructor(private http: HttpClient, private route: ActivatedRoute, private productService: ProductService, private usuarioService: UsuarioService){}
+  
   ngOnInit(): void {
-    const location = { latitude: 36.71781, longitude: -4.28771 };
-    this.initMap(location);
+    this.route.params.subscribe(params => {
+      this.idProducto = params['id'];
+    });
+
+    this.productService.getProductInfo(this.idProducto).subscribe(data1 => {
+      this.idUsuario = data1.vendedor;
+
+      this.usuarioService.getLocation(this.idUsuario).subscribe(data2 => {
+        this.location = {latitude: data2.latitud, longitude: data2.longitud};
+        this.initMap(this.location);
+      })
+      
+    })
   }
 
   private initMap(location: { latitude: number; longitude: number }): void {
     const map = L.map('map').setView([location.latitude, location.longitude], 30);
   
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+    maxZoom: 15,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'  
     }).addTo(map);
   
     L.marker([location.latitude, location.longitude])
       .addTo(map)
-      .bindPopup('Specified Location')
       .openPopup();
   }
   
