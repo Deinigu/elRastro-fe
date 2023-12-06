b1516340a4120e882bc7a42a314fac27cc41c14b
Ravenrex10 <lucas.colbert@hotmail.com>
2023-12-06 20:25:01 +0100

    🔧 Ver precio de la ultima puja de un producto

--- src/app/features/product-info/price/price.component.ts
+++ src/app/features/product-info/price/price.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from '../../../services/product-service/product.service';
+import { PujaService } from '../../../services/puja-service/puja-service.service';
 
 @Component({
   selector: 'app-price',
@@ -10,21 +11,27 @@ import { ProductService } from '../../../services/product-service/product.servic
   imports: [CommonModule],
   templateUrl: './price.component.html',
   styleUrl: './price.component.css',
-  providers: [ProductService]
+  providers: [ProductService, PujaService]
 })
 export class PriceComponent implements OnInit {
   precio = "";
   idProducto = "";
 
-  constructor(private http: HttpClient, private route: ActivatedRoute, private productService: ProductService){}
+  constructor(private http: HttpClient, private route: ActivatedRoute, private productService: ProductService, private pujaService: PujaService){}
 
   ngOnInit(){
     this.route.params.subscribe(params => {
       this.idProducto = params['id'];
     });
 
-    this.productService.getProductInfo(this.idProducto).subscribe(data => {
-      this.precio = data.precio;
-    })
+    this.pujaService.getUltimaPuja(this.idProducto).subscribe((puja) => {
+      if (puja) {
+        this.precio = puja.valor;
+      }else{
+        this.productService.getProductInfo(this.idProducto).subscribe(data => {
+          this.precio = data.precio;
+        })
+      }
+    });
   }
 }
