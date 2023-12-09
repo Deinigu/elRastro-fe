85bee6aef83cd14738b7465d31569ba86d65c5bf
Ravenrex10 <lucas.colbert@hotmail.com>
2023-12-09 22:29:03 +0100

    ✨Borrar chats, ver imagen de chats, acceder a chats

--- src/app/features/chats/mischats/mischats.component.ts
+++ src/app/features/chats/mischats/mischats.component.ts
@@ -17,6 +17,8 @@ interface Conversation {
   vendedor?: Usuario;
   usuario1: string;
   usuario2: string;
+  fotoURL: string;
+  ultimoMensaje: any;
 }
 
 interface Usuario {
@@ -46,6 +48,8 @@ export class MischatsComponent implements OnInit {
   conversacion: any;
   idChat = '';
   mensajeNuevo: string = '';
+  conversacionesNuevas: string[] = [];
+  listaConversaciones: string[] = [];
   
   constructor(
     private http: HttpClient,
@@ -62,52 +66,75 @@ export class MischatsComponent implements OnInit {
 
     this.conversacionService.getAllConversaciones(this.usuarioId).subscribe(data => {
       this.conversaciones = data;
-      this.conversaciones.forEach(conversacion => {
-        // Obtener información del producto
-        const productId = conversacion.productoId;
-        if (productId) {
-          this.productoService.getProductInfo(productId).subscribe(productDetails => {
-            conversacion.productoNombre = productDetails.Nombre;
-            conversacion.vendedor = { id: productDetails.vendedor, nombreUsuario: '' };
-            // Determinar quién es el vendedor y quién es el comprador
-            if (this.usuarioId === conversacion.usuario1 && this.usuarioId === productDetails.vendedor) {
-                conversacion.comprador = { id: conversacion.usuario2, nombreUsuario: '' };
-              }  else if (this.usuarioId !== conversacion.usuario1 && this.usuarioId !== productDetails.vendedor) {
-              conversacion.comprador = { id: conversacion.usuario1, nombreUsuario: '' };
-            } else if (this.usuarioId !== conversacion.usuario1 && this.usuarioId === productDetails.vendedor) {
-              conversacion.comprador = { id: conversacion.usuario1, nombreUsuario: '' };
-            } else {
-              conversacion.comprador = { id: conversacion.usuario2, nombreUsuario: '' };
-            }
+      this.usuarioService.getUsuarioInfo(this.usuarioId).subscribe(usuario => {
+        this.listaConversaciones = usuario.listaConver;
 
-            if (conversacion.vendedor) {
-              this.usuarioService.getUsuarioInfo(conversacion.vendedor.id).subscribe(vendedorDetails => {
-                if (conversacion.vendedor) {
-                  conversacion.vendedor.nombreUsuario = vendedorDetails.nombreUsuario;
-                }
-              });
-            }
-            
-            if (conversacion.comprador) {
-              this.usuarioService.getUsuarioInfo(conversacion.comprador.id).subscribe(compradorDetails => {
-                if (conversacion.comprador) {
-                  conversacion.comprador.nombreUsuario = compradorDetails.nombreUsuario;
-                }
-              }); 
-            }               
-          });
-        }
-      });      
+        this.conversaciones = this.conversaciones.filter(item => this.listaConversaciones.includes(item._id));
+        console.log(this.listaConversaciones);
+        console.log(this.conversaciones);
+        this.conversaciones.forEach(conversacion => {
+          // Obtener información del producto
+          const productId = conversacion.productoId;
+          conversacion.ultimoMensaje = conversacion.chats.at(conversacion.chats.length-1)?.texto;
+          if (productId) {
+            this.productoService.getProductInfo(productId).subscribe(productDetails => {
+              conversacion.fotoURL = productDetails.fotoURL[0];
+              conversacion.productoNombre = productDetails.Nombre;
+              conversacion.vendedor = { id: productDetails.vendedor, nombreUsuario: '' };
+              // Determinar quién es el vendedor y quién es el comprador
+              if (this.usuarioId === conversacion.usuario1 && this.usuarioId === productDetails.vendedor) {
+                  conversacion.comprador = { id: conversacion.usuario2, nombreUsuario: '' };
+                }  else if (this.usuarioId !== conversacion.usuario1 && this.usuarioId !== productDetails.vendedor) {
+                conversacion.comprador = { id: conversacion.usuario1, nombreUsuario: '' };
+              } else if (this.usuarioId !== conversacion.usuario1 && this.usuarioId === productDetails.vendedor) {
+                conversacion.comprador = { id: conversacion.usuario1, nombreUsuario: '' };
+              } else {
+                conversacion.comprador = { id: conversacion.usuario2, nombreUsuario: '' };
+              }
+
+              if (conversacion.vendedor) {
+                this.usuarioService.getUsuarioInfo(conversacion.vendedor.id).subscribe(vendedorDetails => {
+                  if (conversacion.vendedor) {
+                    conversacion.vendedor.nombreUsuario = vendedorDetails.nombreUsuario;
+                  }
+                });
+              }
+              
+              if (conversacion.comprador) {
+                this.usuarioService.getUsuarioInfo(conversacion.comprador.id).subscribe(compradorDetails => {
+                  if (conversacion.comprador) {
+                    conversacion.comprador.nombreUsuario = compradorDetails.nombreUsuario;
+                  }
+                }); 
+              }               
+            });
+          }
+        }); 
+      });     
     });
   }
 
   accederChat(conversacionId: string) {
     // Lógica para acceder al chat
-    console.log('Acceder al chat con ID:', conversacionId);
+    window.location.href = `http://localhost:4200/chats/${conversacionId}`;
   }
 
   borrarChat(conversacionId: string) {
     // Lógica para borrar el chat
     console.log('Borrar el chat con ID:', conversacionId);
+    this.usuarioService.getUsuarioInfo(this.usuarioId).subscribe(usuario => {
+      this.conversacionesNuevas = usuario.listaConver;
+      this.conversacionesNuevas.splice(this.conversacionesNuevas.indexOf(conversacionId), 1);
+      usuario.listaConver = this.conversacionesNuevas;
+      console.log(usuario);
+      this.usuarioService.editarPerfil(this.usuarioId, usuario).subscribe(
+        (respuesta) => {
+          console.log('Perfil actualizado con éxito:', respuesta);
+          window.location.href = `http://localhost:4200/usuario/${this.usuarioId}/chats`;
+        },
+        (error) => {
+          console.error('Error al actualizar el perfil:', error);
+        });;
+    });
   }
 }
