0c56bde917f6f170265acf183f537079f44d1657
Deinigu <diegolopezreduello@gmail.com>
2023-12-09 17:00:05 +0100

    ✨Multiples imágenes placeholder

--- src/app/features/crear-producto/crear-producto.component.ts
+++ src/app/features/crear-producto/crear-producto.component.ts
@@ -4,6 +4,7 @@ import { MaterialModule } from '../../material.module';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ProductService } from '../../services/product-service/product.service';
 import { FormsModule } from '@angular/forms';
+import { ImageService } from '../../services/image-service/image-service.service';
 
 @Component({
   selector: 'app-crear-producto',
@@ -11,17 +12,34 @@ import { FormsModule } from '@angular/forms';
   imports: [CommonModule, FormsModule, MaterialModule, HttpClientModule],
   templateUrl: './crear-producto.component.html',
   styleUrl: './crear-producto.component.css',
-  providers: [ProductService],
+  providers: [ProductService, ImageService],
 
 })
 export class CrearProductoComponent implements OnInit{
+  selectedFiles: File[] = [];
+
   constructor(
     private http: HttpClient,
-    private productService: ProductService
+    private productService: ProductService,
+    private imageService: ImageService
   ) { }
-
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
 
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files) {
+      this.selectedFiles = Array.from(input.files);
+    }
+  }
+
+  onButtonClicked(): void {
+    if (this.selectedFiles.length > 0) {
+      this.imageService.uploadImage(this.selectedFiles).subscribe(response => {
+        console.log('Images uploaded:', response.urls);
+        // Handle the response as needed
+      });
+    }
+  }
 }
