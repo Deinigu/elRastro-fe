bd96a62fbf4183ec08f6a88c752f63b8c7145f62
Deinigu <diegolopezreduello@gmail.com>
2023-12-09 15:00:32 +0100

    ✨Inicializar crear-producto

---
+++ src/app/features/crear-producto/crear-producto.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-crear-producto',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './crear-producto.component.html',
+  styleUrl: './crear-producto.component.css'
+})
+export class CrearProductoComponent {
+
+}
