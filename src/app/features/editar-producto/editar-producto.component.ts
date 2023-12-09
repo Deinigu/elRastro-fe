092b7473081a7601560facfa1407f00043bc6cd2
Deinigu <diegolopezreduello@gmail.com>
2023-12-10 00:46:04 +0100

    ✨Editar producto

--- src/app/features/editar-producto/editar-producto.component.ts
+++ src/app/features/editar-producto/editar-producto.component.ts
@@ -16,8 +16,7 @@ import { FormsModule } from '@angular/forms';
   providers: [ProductService, ImageService]
 })
 export class EditarProductoComponent implements OnInit {
-onSubmit() {
-}
+
   idProducto = "";
   tagsInput: string = "";
   error_empty_field: boolean = false;
@@ -27,6 +26,7 @@ onSubmit() {
   producto_creado: boolean = false;
   producto_en_proceso: boolean = false;
   error_general: boolean = false;
+  datestring : string = '';
 
   producto: Producto = {
     Nombre: '',
@@ -51,16 +51,63 @@ onSubmit() {
     });
 
     this.productService.getProductInfo(this.idProducto).subscribe(data => {
-      this.producto.Nombre = data.Nombre,
-      this.producto.descripcion = data.descripcion,
-      this.producto.fotoURL = data.fotoURL,
-      this.producto.precio = data.precio,
-      this.producto.tags = data.tags,
-      this.producto.cierre = data.tags,
-      this.producto.vendedor = data.vendedor
-    })
+        this.producto.Nombre = data.Nombre,
+        this.producto.descripcion = data.descripcion,
+        this.producto.fotoURL = data.fotoURL,
+        this.producto.precio = data.precio,
+        this.producto.tags = data.tags,
+        this.producto.cierre = data.cierre,
+        this.producto.vendedor = data.vendedor
+
+        this.datestring = new Date(data.cierre).toISOString().slice(0, 16);;
+
+        this.tagsInput = this.producto.tags.join(',');
+      });
   }
-  onButtonVolverClick() : void {
+
+  onSubmit() {
+    this.producto.tags = this.tagsInput.split(',').map(tag => tag.trim());
+    this.producto.cierre = new Date(this.datestring);
+    this.producto.precio = Number(this.producto.precio);
+
+    // Reiniciar errores
+    this.error_empty_field = false;
+    this.error_precio = false;
+    this.error_fotos = false;
+    this.error_fecha = false;
+    this.error_general = false;
+
+    // Get today's date
+    const currentDate = new Date();
+    if (this.producto.cierre <= currentDate) {
+      this.error_fecha = true;
+      return;
+    }
+    else if (this.producto.Nombre.length == 0 || this.producto.descripcion.length == 0 || this.producto.tags.length == 0) {
+      this.error_empty_field = true;
+    } else if (this.producto.precio <= 0) {
+      this.error_precio = true;
+    }
+
+    else {
+      console.log(this.producto);
+      this.producto_en_proceso = true;
+      this.productService.editProducto(this.idProducto, this.producto)
+        .subscribe(
+          (respuesta) => {
+            console.log('Product edited:', respuesta);
+            this.producto_en_proceso = false;
+            this.producto_creado = true;
+          },
+          (error) => {
+            console.error('Error creating product:', error);
+            this.producto_en_proceso = false;
+            this.error_general = true;
+          });
+    }
+  }
+
+  onButtonVolverClick(): void {
     this.router.navigate(['/']);
   }
 }
--- src/app/services/product-service/product.service.ts
+++ src/app/services/product-service/product.service.ts
@@ -28,6 +28,10 @@ export class ProductService {
     const url = 'http://localhost:8001/api/productos/busqueda/' +tags;
     return this.http.get<any>(url);
   }
+  editProducto(idProducto : string, producto: Producto): Observable<any> {
+    const url = "http://localhost:8001/api/productos/update/" + idProducto + "/";
+    return this.http.put(url, producto);
+  }
 }
 
 
