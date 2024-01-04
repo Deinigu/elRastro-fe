0e948e0261f6bb15f17148ce3dda4b94f9277552
Ravenrex10 <lucas.colbert@hotmail.com>
2024-01-04 18:37:02 +0100

    cambio de links

--- src/app/services/product-service/product.service.ts
+++ src/app/services/product-service/product.service.ts
@@ -12,30 +12,30 @@ export class ProductService {
 
   getProductInfo(idProducto: string): Observable<any> {
 
-    const url = 'http://51.21.137.60:8001/api/productos/'+idProducto + '/';
+    const url = 'https://13.38.223.212:8001/api/productos/'+idProducto + '/';
     return this.http.get<any>(url);
   }
 
   getAllProducts() : Observable<any> {
-    const url = 'http://51.21.137.60:8001/api/productos/';
+    const url = 'https://13.38.223.212:8001/api/productos/';
     return this.http.get<any>(url);
   }
 
   createProducto(producto: Producto): Observable<Producto> {
-    const url = 'http://51.21.137.60:8001/api/productos/';
+    const url = 'https://13.38.223.212:8001/api/productos/';
     return this.http.post<Producto>(url, producto);
   }
   
   getSearchProducts(tags : string) : Observable<any> {
-    const url = 'http://51.21.137.60:8001/api/productos/busqueda/' +tags;
+    const url = 'https://13.38.223.212:8001/api/productos/busqueda/' +tags;
     return this.http.get<any>(url);
   }
   editProducto(idProducto : string, producto: Producto): Observable<any> {
-    const url = "http://51.21.137.60:8001/api/productos/" + idProducto + "/";
+    const url = "https://13.38.223.212:8001/api/productos/" + idProducto + "/";
     return this.http.put(url, producto);
   }
   deleteProducto(productoID: string): Observable<any> {
-    const url = 'http://51.21.137.60:8001/api/productos/'+productoID + "/";
+    const url = 'https://13.38.223.212:8001/api/productos/'+productoID + "/";
     return this.http.delete<any>(url);
   }
 }
--- src/app/services/puja-service/puja-service.service.ts
+++ src/app/services/puja-service/puja-service.service.ts
@@ -11,24 +11,24 @@ export class PujaService {
 
   getUltimaPuja(idProducto: string): Observable<any> {
     const url =
-      'http://51.21.137.60:8002/api/pujas/ultima_puja/producto/' +
+      'https://13.38.223.212:8002/api/pujas/ultima_puja/producto/' +
       idProducto +
       '/';
     return this.http.get<any>(url);
   }
 
   createPuja(puja: Puja): Observable<Puja> {
-    const url = 'http://51.21.137.60:8002/api/pujas/';
+    const url = 'https://13.38.223.212:8002/api/pujas/';
     return this.http.post<Puja>(url, puja);
   }
 
   getPujasByUser(idUser: string): Observable<any> {
-    const url = 'http://51.21.137.60:8002/api/pujas/usuario/' + idUser + '/';
+    const url = 'https://13.38.223.212:8002/api/pujas/usuario/' + idUser + '/';
     return this.http.get<any>(url);
   }
 
   putPujaPagada(idPuja: string): Observable<any> {
-    const url = 'http://51.21.137.60:8002/api/pujas/pagada/';
+    const url = 'https://13.38.223.212:8002/api/pujas/pagada/';
     const body = { idPuja: idPuja };
     return this.http.put<any>(url, body);
   }
